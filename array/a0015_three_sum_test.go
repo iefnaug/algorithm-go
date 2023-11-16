5f0a0ec3c291d51b3c2280102df6712ba361b2b0
guanfei <guanfei@51credit.com>
2023-11-16 11:05:29 +0800

    [lc-15]三数和

---
+++ array/a0015_three_sum_test.go
@@ -0,0 +1,126 @@
+package array
+
+import (
+	"fmt"
+	"sort"
+	"testing"
+)
+
+/*
+给你一个整数数组 nums ，判断是否存在三元组 [nums[i], nums[j], nums[k]] 满足 i != j、i != k 且 j != k ，同时还满足 nums[i] + nums[j] + nums[k] == 0 。请
+
+你返回所有和为 0 且不重复的三元组。
+
+注意：答案中不可以包含重复的三元组。
+
+
+示例 1：
+
+输入：nums = [-1,0,1,2,-1,-4]
+输出：[[-1,-1,2],[-1,0,1]]
+解释：
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0 。
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0 。
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0 。
+不同的三元组是 [-1,0,1] 和 [-1,-1,2] 。
+注意，输出的顺序和三元组的顺序并不重要。
+示例 2：
+
+输入：nums = [0,1,1]
+输出：[]
+解释：唯一可能的三元组和不为 0 。
+示例 3：
+
+输入：nums = [0,0,0]
+输出：[[0,0,0]]
+解释：唯一可能的三元组和为 0 。
+
+
+提示：
+
+3 <= nums.length <= 3000
+-10^5 <= nums[i] <= 10^5
+*/
+
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	var ret [][]int
+	l := len(nums)
+	for i := 0; i < l-2; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		target := 0 - nums[i]
+		left := i + 1
+		right := l - 1
+		for left < right {
+			v := nums[left] + nums[right]
+			if v < target {
+				left++
+				//for left < right && nums[left] == nums[left-1] {
+				//	left++
+				//}
+			} else if v == target {
+				ret = append(ret, []int{nums[i], nums[left], nums[right]})
+				left++
+				right--
+				for left < right && nums[left] == nums[left-1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right+1] {
+					right--
+				}
+			} else {
+				right--
+				//for left < right && nums[right] == nums[right+1] {
+				//	right--
+				//}
+			}
+		}
+	}
+	return ret
+}
+
+func threeSum2(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举 a
+	for first := 0; first < n; first++ {
+		// 需要和上一次枚举的数不相同
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+		// c 对应的指针初始指向数组的最右端
+		third := n - 1
+		target := -1 * nums[first]
+		// 枚举 b
+		for second := first + 1; second < n; second++ {
+			// 需要和上一次枚举的数不相同
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+			// 需要保证 b 的指针在 c 的指针的左侧
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			// 如果指针重合，随着 b 后续的增加
+			// 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+			if second == third {
+				break
+			}
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
+
+func TestThreeSum(t *testing.T) {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	ret := threeSum(nums)
+	fmt.Println(ret)
+
+}
